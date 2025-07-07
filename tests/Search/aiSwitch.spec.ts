31bd63d2267f5baff2abfda2ba49b189c95f8c89
Jason Murray <umurja@gmail.com>
2025-07-07 11:52:33 -0400

    adding new folder and test for search, AI switch

---
+++ tests/Search/aiSwitch.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('AI search enabled by default', async ({ page }) => {
+  await page.goto('https://world.optimizely.com/');
+  const defaultSearchModeLabel = await page.locator('#search-toggle-container > span > span.ai');
+  expect(defaultSearchModeLabel).toHaveText('AI On');
+});
