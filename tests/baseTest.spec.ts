211849b7b3d9dbcf37a9b4f96914a112589d872e
Jason Murray <umurja@gmail.com>
2025-07-07 10:19:29 -0400

    test commit message

--- tests/baseTest.spec.ts
+++ tests/baseTest.spec.ts
@@ -3,4 +3,5 @@ import { test, expect } from '@playwright/test';
 test('homepage loads', async ({ page }) => {
   await page.goto('/'); // thanks to baseURL, this goes to https://world.optimizely.com/
   await expect(page).toHaveTitle(/Optimizely Developer Community/);
+  //comment - this can be removed
 });
