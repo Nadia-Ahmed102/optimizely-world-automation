46456fba0bc16d749a45da878e516cbeae007bdd
Jason Murray <umurja@gmail.com>
2025-07-09 06:57:38 -0400

    random comment made

--- tests/Search/aiSwitch.spec.ts
+++ tests/Search/aiSwitch.spec.ts
@@ -4,4 +4,5 @@ test('AI search enabled by default', async ({ page }) => {
   await page.goto('https://world.optimizely.com/');
   const defaultSearchModeLabel = await page.locator('#search-toggle-container > span > span.ai');
   expect(defaultSearchModeLabel).toHaveText('AI On');
+  //comment - delete this later
 });
