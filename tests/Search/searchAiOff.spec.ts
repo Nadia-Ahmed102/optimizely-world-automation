f15f025eb084f94e20cf3a20d0d03b873de3a7d7
Amatuz <amatuzzzahura@gmail.com>
2025-07-29 20:42:02 +0600

    Search with AI Off

---
+++ tests/Search/searchAiOff.spec.ts
@@ -0,0 +1,28 @@
+﻿import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    await page.goto('https://world.optimizely.com/');
+
+    // Wait for the span with text "AI Button" to be visible
+    const aiToggle = page.locator('span:text("AI On")');
+    test.setTimeout(3000000);
+    await expect(aiToggle).toBeVisible();
+
+    //Click the span to turn AI "Off"
+    await aiToggle.click();
+
+    console.log('✅ AI is Off successfully');
+
+    const searchInput = page.locator('input[placeholder="Search World"]');
+    await searchInput.click();
+    await searchInput.fill('Optimizely');
+    await page.keyboard.press('Enter');
+    test.setTimeout(3000000);
+
+    await page.waitForLoadState('domcontentloaded');
+    await page.waitForLoadState('networkidle');
+    
+
+
+
+});
