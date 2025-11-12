32b808b2003abb223e896a53892cfbab8917ee2d
ufimranopti <umarfaruqueibne.ariff@optimizely.com>
2025-11-12 20:56:14 +0600

    Updated the omvpLanding test using the POM, Added 4 new assertions here.

--- Pages/omvpPage.ts
+++ Pages/omvpPage.ts
@@ -2,25 +2,41 @@ import { Page, Locator } from "@playwright/test";
 import { basePage } from "./BasePage";
 
 export class omvpPage extends basePage {
-  // Accordion
   readonly page: Page;
+
+  // Accordion
   readonly firstAccordionFirstText: Locator;
   readonly firstAccordionSecondText: Locator;
   readonly firstAccordionRightArrow: Locator;
-  readonly firstAccordionLeftArrow: Locator;
-  readonly secondAccordionFirstText: Locator;
-  readonly secondAccordionSecondText: Locator;
-  readonly secondAccordionRightArrow: Locator;
-  readonly secondAccordionLeftArrow: Locator;
-  static firstAccordionFirstText: any;
 
-  // FAQ locators
-  readonly firstFAQ: Locator;
-  readonly firstFAQToggle: Locator;
+  // Hero
+  readonly heroText: Locator;
+
+  // Breadcrumbs
+  readonly homeBreadcrumb: Locator;
+  readonly communityBreadcrumb: Locator;
+
+  // FAQ
+  readonly faqQuestion: Locator;
+  readonly faqAnswer: Locator;
+
+  // Application & Nomination CTAs
+  readonly applyTechCTA: Locator;
+  readonly applyStrategyCTA: Locator;
+  readonly nominateCTA: Locator;
+
+  // Explore OMVP section
+  readonly exploreBecomeMember: Locator;
+  readonly exploreBenefits: Locator;
+  readonly exploreEvents: Locator;
+  readonly exploreOMVPMembers: Locator;
+  readonly exploreApplicationNomination: Locator;
 
   constructor(page: Page) {
     super(page);
     this.page = page;
+
+    // Accordion
     this.firstAccordionFirstText = page.locator(
       "#carousel-277085 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > p:nth-child(1) > em:nth-child(1)"
     );
@@ -30,28 +46,28 @@ export class omvpPage extends basePage {
     this.firstAccordionRightArrow = page.locator(
       "#carousel-277085 > a:nth-child(4) > span:nth-child(1)"
     );
-    this.firstAccordionLeftArrow = page.locator(
-      "body > div:nth-child(9) > div:nth-child(1) > div:nth-child(4) > section:nth-child(1) > a:nth-child(3)"
-    );
-    this.secondAccordionFirstText = page.locator(
-      "div[class='carousel-item hideMobile active'] em"
-    );
-    this.secondAccordionSecondText = page.locator(
-      "section[id='carousel-277091'] div[class='carousel-item hideMobile active'] em"
-    );
-    this.secondAccordionRightArrow = page.locator(
-      "section[id='carousel-277091'] span[class='carousel-control-next-icon']"
-    );
-    this.secondAccordionLeftArrow = page.locator(
-      "section[id='carousel-277091'] span[class='carousel-control-prev-icon']"
-    );
 
-    // FAQ locators
-    this.firstFAQ = page.locator(
-      "#heading-accordion-277086-1 > div > div.symbol > div.plusminus"
-    );
-    this.firstFAQToggle = page.locator('//*[@id="accordion-277086"]/div[2]');
+    // Hero
+    this.heroText = page.locator("h1.bold > span:nth-child(1)");
+
+    // Breadcrumbs
+    this.homeBreadcrumb = page.locator('#breadcrumbs > span:nth-child(1) > a:nth-child(1)');
+    this.communityBreadcrumb = page.locator('#breadcrumbs > a:nth-child(2)');
+
+    // FAQ
+    this.faqQuestion = page.locator("#heading-accordion-277086-1 > div:nth-child(1) > div:nth-child(2) > h5:nth-child(1)");
+    this.faqAnswer = page.locator("#accordion-277086-1 > div:nth-child(1) > p:nth-child(1)");
+
+    // Application & Nomination CTAs
+    this.applyTechCTA = page.locator('div.adjustheight-75:nth-child(1) > div:nth-child(1) > section:nth-child(1) > div:nth-child(2) > div:nth-child(1) > p:nth-child(3) > a:nth-child(1)');
+    this.applyStrategyCTA = page.locator('div.adjustheight-75:nth-child(1) > div:nth-child(1) > section:nth-child(1) > div:nth-child(2) > div:nth-child(1) > p:nth-child(4) > a:nth-child(1)');
+    this.nominateCTA = page.locator('div.adjustheight-75:nth-child(2) > div:nth-child(1) > section:nth-child(1) > div:nth-child(2) > div:nth-child(1) > p:nth-child(3) > a');
+
+    // Explore OMVP
+    this.exploreBecomeMember = page.locator("div.adjustheight-100:nth-child(1) > div:nth-child(1) > section:nth-child(1) > a:nth-child(1)");
+    this.exploreBenefits = page.locator("div.adjustheight-100:nth-child(2) > div:nth-child(1) > section:nth-child(1) > a:nth-child(1)");
+    this.exploreEvents = page.locator("div.col:nth-child(3) > div:nth-child(1) > section:nth-child(1) > a:nth-child(1)");
+    this.exploreOMVPMembers = page.locator("div.col:nth-child(4) > div:nth-child(1) > section:nth-child(1) > a:nth-child(1)");
+    this.exploreApplicationNomination = page.locator("div.col:nth-child(5) > div:nth-child(1) > section:nth-child(1) > a:nth-child(1)");
   }
-  
-  
 }
--- tests/omvp/omvpLanding.spec.ts
+++ tests/omvp/omvpLanding.spec.ts
@@ -1,10 +1,8 @@
 import { test, expect } from "@playwright/test";
 import { omvpPage } from "../../Pages/omvpPage";
 
-
 test.beforeEach(async ({ page }) => {
   const omvp = new omvpPage(page);
-  // Navigate to the OMVP page before each test
   await page.goto("https://world.optimizely.com/community/omvp/");
 });
 
@@ -14,21 +12,60 @@ test("OMVP page should have expected title", async ({ page }) => {
 
 test("Test accordion text changes on click", async ({ page }) => {
   const omvp = new omvpPage(page);
-  const firstAccordion = omvp.firstAccordionFirstText;
-  await expect(firstAccordion).toBeVisible();
+  await expect(omvp.firstAccordionFirstText).toBeVisible();
   await omvp.firstAccordionRightArrow.click();
-  const secondAccordion = omvp.firstAccordionSecondText;
-  await expect(secondAccordion).toBeVisible();
-  await expect(firstAccordion).not.toBeVisible();
+  await expect(omvp.firstAccordionSecondText).toBeVisible();
+  await expect(omvp.firstAccordionFirstText).not.toBeVisible();
 });
 
-test("Click first faq", async ({ page }) => {
+test("OMVP page hero text should be correct", async ({ page }) => {
   const omvp = new omvpPage(page);
-  await expect(omvp.firstFAQ).toBeVisible();
-  await expect(omvp.firstFAQ).not.toHaveClass(/show/);
-  await omvp.firstFAQToggle.click();
-  await expect(omvp.firstFAQ).toHaveClass(/show/);
-  await expect(omvp.firstFAQ).toContainText(
+  await expect(omvp.heroText).toBeVisible();
+  await expect(omvp.heroText).toHaveText("Optimizely Most Valuable Professionals");
+});
+
+test("OMVP Benefits page breadcrumbs should be correct", async ({ page }) => {
+  const omvp = new omvpPage(page);
+  await expect(omvp.homeBreadcrumb).toBeVisible();
+  await expect(omvp.homeBreadcrumb).toHaveText("Home");
+  await expect(omvp.homeBreadcrumb).toHaveAttribute("href", "/");
+
+  await expect(omvp.communityBreadcrumb).toBeVisible();
+  await expect(omvp.communityBreadcrumb).toHaveText("Community");
+  await expect(omvp.communityBreadcrumb).toHaveAttribute("href", "/community/");
+});
+
+test("OMVP FAQ accordion should expand and show correct text", async ({ page }) => {
+  const omvp = new omvpPage(page);
+  await expect(omvp.faqQuestion).toBeVisible();
+  await expect(omvp.faqQuestion).toHaveText("What to expect in a nomination or first-time application process?");
+  await omvp.faqQuestion.click({ force: true });
+  await expect(omvp.faqAnswer).toBeVisible();
+  await expect(omvp.faqAnswer).toHaveText(
     "Once an applicant applies, they can expect to be notified when their application has been received. Once the application period closes, the selection process begins. Optimizely chooses OMVPs based upon the application requirements and the OMVP will be notified of acceptance or denial of entry into the program, as well as their rank/tier. The process can take about a month to complete."
   );
+});
+
+test("OMVP Benefits page Application and Nomination CTAs should redirect correctly", async ({ page }) => {
+  const omvp = new omvpPage(page);
+  await expect(omvp.applyTechCTA).toBeVisible();
+  await expect(omvp.applyTechCTA).toHaveText("Apply for a Technology OMVP now");
+  await expect(omvp.applyTechCTA).toHaveAttribute("href", "https://forms.office.com/r/0Xim01z897");
+
+  await expect(omvp.applyStrategyCTA).toBeVisible();
+  await expect(omvp.applyStrategyCTA).toHaveText("Apply for a Strategy OMVP now");
+  await expect(omvp.applyStrategyCTA).toHaveAttribute("href", "https://forms.office.com/r/ss0S8q7n34");
+
+  await expect(omvp.nominateCTA).toBeVisible();
+  await expect(omvp.nominateCTA).toHaveText("Nominate now");
+  await expect(omvp.nominateCTA).toHaveAttribute("href", "https://forms.office.com/r/7crUwMLJPh");
+});
+
+test("Explore OMVP section links should redirect correctly", async ({ page }) => {
+  const omvp = new omvpPage(page);
+  await expect(omvp.exploreBecomeMember).toHaveAttribute("href", "/community/omvp/become-a-member/");
+  await expect(omvp.exploreBenefits).toHaveAttribute("href", "/community/omvp/benefits/");
+  await expect(omvp.exploreEvents).toHaveAttribute("href", "/community/omvp/events/");
+  await expect(omvp.exploreOMVPMembers).toHaveAttribute("href", "/community/omvp/members/");
+  await expect(omvp.exploreApplicationNomination).toHaveAttribute("href", "/community/omvp/application-and-nomination/");
 });
